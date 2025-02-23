ea1d22bc0603eca379faae54719c7db284e416e7
Natalia Kur <63752791+Katalia91@users.noreply.github.com>
2025-02-23 16:28:33 +0100

    Improve the prompt context

--- src/index.js
+++ src/index.js
@@ -1,28 +1,26 @@
 function displayPoem(response) {
-  new Typewriter('#poem-content', {
+  new Typewriter("#poem-content", {
     strings: response.data.answer,
     autoStart: true,
     delay: 50,
-    cursor: ""
+    cursor: "",
   });
 }
 
-
 function generatePoem(event) {
-    event.preventDefault();
-    let poemContent = document.getElementById("poem-content");
-    let userInput = document.querySelector("#prompt").value;
+  event.preventDefault();
+  let poemContent = document.getElementById("poem-content");
+  let userInput = document.querySelector("#prompt").value;
 
-    const apiKey = "bab7d62a3b8o5fe03ce481f0a26t86db";
-    let context = `You are an AI assistant and poet. Please generate a poem about the chosen topic. It should have not more than 5 lines by default. It should be in the following format, so that each line is a separate <p></p> like this: <p>First line</p></p>Second line</p> and so on. After finished poem use <br /><br /> and sign with <em> -Your personal poet</em>🖋️`
-    let prompt = `Generate a poem about ${userInput}`;
-    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  const apiKey = "bab7d62a3b8o5fe03ce481f0a26t86db";
+  let context = `You are an AI assistant and poet. Please generate a poem about the chosen topic. It should have not more than 6 lines by default, but these max 6 lines should be a finished poen, it shouldn't just stop with a comma. It should be in the following format, so that each line is a separate <p></p> like this: <p>First line</p></p>Second line</p> and so on. After finished poem use <br /><br /> and sign with <em> -Your personal poet</em>🖋️`;
+  let prompt = `Generate a poem about ${userInput}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-    poemContent.classList.toggle("hidden");
-    poemContent.innerHTML = `<span class="blink">⏳ Generating a poem about: ${userInput}...</span>`;
+  poemContent.classList.toggle("hidden");
+  poemContent.innerHTML = `<span class="blink">⏳ Generating a poem about: <strong>${userInput}</strong>...</span>`;
 
-    axios.get(apiUrl).then(displayPoem);
-
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
