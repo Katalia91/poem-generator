8fd9ef590a94207a1807207c607a3c75c1007d3d
Natalia Kur <natalia.kur@yahoo.com>
2025-08-24 19:22:11 +0200

    Change poem generator to running plan generator

--- src/index.js
+++ src/index.js
@@ -13,12 +13,12 @@ function generatePoem(event) {
   let userInput = document.querySelector("#prompt").value;
 
   const apiKey = "bab7d62a3b8o5fe03ce481f0a26t86db";
-  let context = `You are an AI assistant and poet. Please generate a poem about the chosen topic. It should have not more than 6 lines by default, but these max 6 lines should be a finished poen, it shouldn't just stop with a comma. It should be in the following format, so that each line is a separate <p></p> like this: <p>First line</p></p>Second line</p> and so on. After finished poem use <br /><br /> and sign with <em> -Your personal poet</em>🖋️`;
-  let prompt = `Generate a poem about ${userInput}`;
+  let context = `You are an AI assistant and a running coach. Please generate a running plan based on the instructions provided. The user can choose from plans for 5k, 10k, half-marathon and marathon, and time they have during the week for excercise (eg. 3x, 4x or 5x week). It should be a finished running plan in a short version, each day should be in a separate line, and the title (for example "Plan for 5km, training 3x week,) should be <h1>, then description for weeks should be <h2>, for example <h2>Week 1</h2>. Generate max 4 weeks at once. for example: <h1>Plan for 5km, training 3x week</h1><h2>Week 1</h2><ul><li>monday: 2km warm-up, 10x100m uphill, 1km cool-down</li><li>Tuesday>...</li> etc.). After finished plan use <br /><br /> and sign with <p>Stay consistent, trust the process, and watch your progress build with every run. 💪✨</p><em> -Your personal coach</em> 🏃‍♀️`;
+  let prompt = `Generate a running plan based on these instructions: ${userInput}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   poemContent.classList.toggle("hidden");
-  poemContent.innerHTML = `<span class="blink">⏳ Generating a poem about: <strong>${userInput}</strong>...</span>`;
+  poemContent.innerHTML = `<span class="blink">⏳ Generating a running plan for: <strong>${userInput}</strong>...</span>`;
 
   axios.get(apiUrl).then(displayPoem);
 }
